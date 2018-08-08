840d306b9f2d86f2c557014187d0f2d0bb2e96f5
jumomma <jumomma@gmail.com>
2018-08-07 20:46:12 -0400

    checkpoint-rails-crud2 complete

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -19,20 +19,59 @@ RSpec.describe PostsController, type: :controller do
     end
   end
 
-# #10 we comment out the tests for show, new, and edit since we won't write the implementation until later.
-  #  describe "GET show" do
-  #    it "returns http success" do
-  #      get :show
-  #      expect(response).to have_http_status(:success)
-  #    end
-  #  end
+  describe "GET show" do
+      it "returns http success" do
+  # #16
+        get :show, {id: my_post.id}
+        expect(response).to have_http_status(:success)
+      end
+      it "renders the #show view" do
+  # #17
+        get :show, {id: my_post.id}
+        expect(response).to render_template :show
+      end
 
-  # describe "GET new" do
-  #   it "returns http success" do
-  #     get :new
-  #     expect(response).to have_http_status(:success)
-  #   end
-  # end
+      it "assigns my_post to @post" do
+        get :show, {id: my_post.id}
+  # #18
+        expect(assigns(:post)).to eq(my_post)
+      end
+    end
+
+  # #1
+   describe "GET new" do
+     it "returns http success" do
+       get :new
+       expect(response).to have_http_status(:success)
+     end
+
+# #2
+     it "renders the #new view" do
+       get :new
+       expect(response).to render_template :new
+     end
+
+# #3
+     it "instantiates @post" do
+       get :new
+       expect(assigns(:post)).not_to be_nil
+     end
+   end
+
+   describe "POST create" do
+     it "increases the number of Post by 1" do
+       expect{ post :create, params: { post: { title: RandomData.random_sentence, body: RandomData.random_paragraph } } }.to change(Post,:count).by(1)
+     end
+
+     it "assigns the new post to @post" do
+       post :create, params: { post: { title: RandomData.random_sentence, body: RandomData.random_paragraph } }
+     end
+
+     it "redirects to the new post" do
+       post :create, params: { post: { title: RandomData.random_sentence, body: RandomData.random_paragraph } }
+       expect(response).to redirect_to Post.last
+     end
+   end
 
   #  describe "GET edit" do
   #    it "returns http success" do
