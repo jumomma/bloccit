a8d1a4d3dc606d214410d6fb00c2b899ec23477e
jumomma <jumomma@gmail.com>
2018-09-21 08:58:14 -0400

    assignment-rails-morecrud complete

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ApplicationRecord
   # has_many method allows a post instance to have many comments related to it
   # and also provides methods that allow us to retrieve comments that belong to a post
-  has_many :comments
+  has_many :comments, dependent: :destroy
 end
